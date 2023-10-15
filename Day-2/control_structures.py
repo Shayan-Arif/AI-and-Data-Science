9386b69b27fdcd7224370bbb8a5e54d23fe3c8b7
Muhammad Shayan <21sw067@students.muet.edu.pk>
2023-10-15 23:28:57 +0100

    basicsPython

---
+++ Day-2/control_structures.py
@@ -0,0 +1,26 @@
+x = 1
+
+if x > 0:
+    print("x is positive")
+
+elif x < 0:
+    print("x is negative")
+else:
+    print("x is zero")
+    
+# For loop
+for i in range(5):
+    print(i)
+print("----------------------------------------------")
+
+# While loop
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+list1 = [1,2,3,4,5,6,7,8,9,10]
+print("----------------------------------------------")
+
+for i in list1:
+    print(i)
---
+++ Python_Basics/BasicMethods/BandName_generator.py
@@ -0,0 +1,4 @@
+print("Assalamu alaikum")
+city = input("Enter the city you grew up in: \n")
+pet = input("Enter the name of your pet: \n")  
+print("Your band name could be -> " + city + " " + pet) 
