f50f0b4d4565d9589f427ba25057460c457c3b83
Shayan-Arif <21sw067@students.muet.edu.pk>
2023-12-24 02:34:02 +0500

    Session 1 of python

--- 0. Python_Basics/1.BasicMethods/BandName_generator.py
+++
@@ -1,4 +0,0 @@
-print("Assalamu alaikum")
-city = input("Enter the city you grew up in: \n")
-pet = input("Enter the name of your pet: \n")  
-print("Your band name could be -> " + city + " " + pet) 
