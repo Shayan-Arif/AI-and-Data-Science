d0b48cfe108803c18bda7aff256653b5840cf49e
Shayan-Arif <21sw067@students.muet.edu.pk>
2024-06-05 06:30:14 +0500

    python

--- 0. Python_Basics/simple/2.Understanding_Data_Types_and_How_to_Manipulate_Strings/BMI_Calculator.py
+++
@@ -1,7 +0,0 @@
-print("BMI calculator")
-weight = float(input("Enter your weight in kg: \n"))
-height = float (input("Enter your height in m: \n"))
-bmi = weight / (height ** 2)
-print("Your BMI is: " + str(bmi))
-
-#print(type(bmi))
